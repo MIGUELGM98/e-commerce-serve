a8a67ba27e9c13c90c1d32b8f2fb4d13be4e2377
MIGUELGM98 <gonzalez.martinez.16040@itsmante.edu.mx>
2021-04-15 10:01:10 -0500

    proyect full

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07145068bda877b37485b84bf2369b18'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7cd79c5d-9bc2-4ace-b98f-5dcfabea0025'
+};
